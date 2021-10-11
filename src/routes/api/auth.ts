c54c05e1acbfda6ac621793727461f07206654f5
KoGyeongtae <kokt0203@naver.com>
2021-10-11 22:47:14 +0900

    ADD - api form

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -4,6 +4,22 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     res.send('auth router');
+});
+
+router.post('/login', async (req, res) => {
+    res.send('login');
+});
+
+router.post('/signup', async (req, res) => {
+    res.send('signup');
+});
+
+router.get('/logout', async (req, res) => {
+    res.send('logout');
+});
+
+router.put('/token', async (req, res) => {
+    res.send('refresh token');
 })
 
 export default router;
--- src/routes/api/post.ts
+++ src/routes/api/post.ts
@@ -2,8 +2,24 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/:pid', async (req, res) => {
     res.send('post router');
-})
+});
+
+router.post('/', async (req, res) => {
+    res.send('create');
+});
+
+router.put('/:pid', async (req, res) => {
+    res.send('update');
+});
+
+router.delete('/:pid', async (req, res) => {
+    res.send('delete');
+});
+
+router.get('/list', async (req, res) => {
+    res.send('list')
+});
 
 export default router;
